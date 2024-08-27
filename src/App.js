f5b5966aed46143efd458e6bd36a43263a21920d
KhaPahm <khapham.work@gmail.com>
2024-08-27 11:15:06 +0700

    kha test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Chào mừng đến với trang web của Hoàng Kha
         </p>
         <a
           className="App-link"
